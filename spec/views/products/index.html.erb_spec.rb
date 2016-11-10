4c78edcc6487b351899dd55d6e7e62dafd19cf25
Timothy Campbell <tim@tandk.dyndns.org>
2016-11-10 11:25:59 +1030

    Implement currency rendering. All tests now pass.

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe "products/index", type: :view do
     render
     assert_select "tr>td", :text => "Name".to_s, :count => 2
     assert_select "tr>td", :text => "Product".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
+    assert_select "tr>td", :text => "$9.99".to_s, :count => 2
     assert_select "tr>td", :text => nil.to_s, :count => 2
   end
 end
