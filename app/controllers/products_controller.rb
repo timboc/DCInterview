2057c4e6822aa95564bc65f49a954766939f231c
Timothy Campbell <tim@tandk.dyndns.org>
2016-11-11 17:13:56 +1030

    Fix some inconsistencies between products and categories views.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.all.order(:name).page(params[:page]).per(5)
   end
 
   # GET /products/1
--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "products/index", type: :view do
   before(:each) do
     category = Category.new(name: "theCategory")
-    assign(:products, [
+    assign(:products, Kaminari.paginate_array([
       Product.create!(
         :name => "Name",
         :product_id => "Product",
@@ -16,14 +16,14 @@ RSpec.describe "products/index", type: :view do
         :price => "9.99",
         :category => category
       )
-    ])
+    ]).page.per(1))
   end
 
   it "renders a list of products" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Product".to_s, :count => 2
-    assert_select "tr>td", :text => "$9.99".to_s, :count => 2
-    assert_select "tr>td", :text => "theCategory".to_s, :count => 2
+    assert_select "tr>td", :text => "Name".to_s, :count => 1
+    assert_select "tr>td", :text => "Product".to_s, :count => 1
+    assert_select "tr>td", :text => "$9.99".to_s, :count => 1
+    assert_select "tr>td", :text => "theCategory".to_s, :count => 1
   end
 end
